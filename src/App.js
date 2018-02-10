8c3c4dafed8b8bb0607223da5dfa5ef7d60d4c5b
Nilo Ballener <nballener@gmail.com>
2018-02-11 09:26:30 +1100

    F2: Add shortid and index pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import shortid from 'shortid'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Home from './components/Home';
+import About from './components/About';
+import CaseStudies from './components/CaseStudies';
+import Portfolio from './components/Portfolio';
+import Blog from './components/Blog';
+
+const links = [
+  { id: shortid.generate(), path: '/', name: 'Home' },
+  { id: shortid.generate(), path: '/about', name: 'About' },
+  { id: shortid.generate(), path: '/casestudies', name: 'Case Studies' },
+  { id: shortid.generate(), path: '/portfolio', name: 'Portfolio' },
+  { id: shortid.generate(), path: '/blog', name: 'Blog' },
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            {links.map(link => (
+              <li key={link.id}>
+                <Link to={link.path}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Router>
     );
   }
 }
